b966fd16c838ea284dea40a8d01c98a6e25aedd7
RidwanA123 <58993858+RidwanA123@users.noreply.github.com>
2023-01-31 23:13:43 -0500

    New Steering

--- RemoteUnit/Controller/Controller.ino
+++ RemoteUnit/Controller/Controller.ino
@@ -3,10 +3,15 @@
 #include <RF24.h>
 
 RF24 radio(7, 8); // CE, CSN
+int acButton = 2;
+int brButton = 3;
 
 const byte address[6] = "00001";
 
 void setup() {
+  pinMode(acButton, INPUT_PULLUP);
+  Serial.begin(9600);
+  pinMode(brButton, INPUT_PULLUP);
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
@@ -14,7 +19,24 @@ void setup() {
 }
 
 void loop() {
-  const char text[] = "1";
+  if (digitalRead(acButton) == LOW) {
+     const char text[] = "1";
+     Serial.println("A");
   radio.write(&text, sizeof(text));
-  delay(1000);
+  }
+  
+   else if(digitalRead(brButton) == LOW) {
+        const char text[] = "2";
+         Serial.println("B");
+  radio.write(&text, sizeof(text));
+    }
+   else {
+      const char text[] = "0";
+       Serial.println("OFF");
+  radio.write(&text, sizeof(text));
+   }
+
+  
+
+
 }
